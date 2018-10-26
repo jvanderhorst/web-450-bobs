ab3f9686216656ec54d8621b530c9b94b95eaaab
Heather <hpeterson@my.bellevue.edu>
2018-10-26 13:31:18 -0400

    added confirmPassword string

--- bobs-computer-repair/src/app/models/user.ts
+++ bobs-computer-repair/src/app/models/user.ts
@@ -1,15 +1,16 @@
-export class User{
+export class User {
     userId: String;
     firstName: String;
     lastName: String;
     email: String;
     username: String;
     password: String;
+    confirmPassword: String;
     phoneNumber: String;
     address: String;
     city: String;
     state: String;
-    zipCode: Number; 
+    zipCode: Number;
     dateCreated: Date;
     dateUpdated: Date;
     dateDeleted: Date;
