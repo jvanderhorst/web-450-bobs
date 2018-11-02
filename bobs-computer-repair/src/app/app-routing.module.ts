53092fb70c58681020bf01313b257a68b50b69b3
Heather <hpeterson@my.bellevue.edu>
2018-11-01 23:10:05 -0400

    routing about and contact pages

--- bobs-computer-repair/src/app/app-routing.module.ts
+++ bobs-computer-repair/src/app/app-routing.module.ts
@@ -6,13 +6,17 @@ import { RegisterComponent } from './register/register.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { SecurityQuestionsComponent } from './security-questions/security-questions.component';
 import { ManagementComponent } from './management/management.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent},
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'management', component: ManagementComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'reset-password', component: ResetPasswordComponent },
   { path: 'security-questions', component: SecurityQuestionsComponent },
-  { path: 'management', component: ManagementComponent },
   { path: '**', redirectTo: '' }
 ];
 
